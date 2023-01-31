5bec5da93e69617e3314ea1ed4f669f9fc317030
MohammmedAhmed8 <94786411+MohammmedAhmed8@users.noreply.github.com>
2023-01-31 16:15:21 +0200

    Add files via upload

---
+++ Session 1/Practice 1/Stripes.java
@@ -0,0 +1,114 @@
+import java.util.*;
+import java.io.*;
+public class Stripes{
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while(t-->0){
+            char[][] c = new char[8][8];
+            for(int i=0; i<8 ;i++)
+                c[i] = sc.next().toCharArray();
+            boolean f = false;
+            for(int i=0; i<8 ;i++){
+                boolean ff = true;
+                for(int j=0; j<8 ;j++) {
+                    if (c[i][j] != 'R')
+                        ff = false;
+                }
+                if(ff)
+                    f = true;
+            }
+            pw.println(f? "R" : "B");
+        }
+        pw.flush();
+    }
+    static class Scanner {
+
+        StringTokenizer st;
+        BufferedReader br;
+        public Scanner(InputStream s) {
+            br = new BufferedReader(new InputStreamReader(s));
+        }
+        public Scanner(String file) throws IOException {
+            br = new BufferedReader(new FileReader(file));
+        }
+        public Scanner(FileReader r) {
+            br = new BufferedReader(r);
+        }
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreTokens())
+                st = new StringTokenizer(br.readLine());
+            return st.nextToken();
+        }
+        public String readAllLines(BufferedReader reader) throws IOException {
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+                content.append(System.lineSeparator());
+            }
+            return content.toString();
+        }
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+        public String nextLine() throws IOException {
+            return br.readLine();
+        }
+        public double nextDouble() throws IOException {
+            String x = next();
+            StringBuilder sb = new StringBuilder("0");
+            double res = 0, f = 1;
+            boolean dec = false, neg = false;
+            int start = 0;
+            if (x.charAt(0) == '-') {
+                neg = true;
+                start++;
+            }
+            for (int i = start; i < x.length(); i++)
+                if (x.charAt(i) == '.') {
+                    res = Long.parseLong(sb.toString());
+                    sb = new StringBuilder("0");
+                    dec = true;
+                } else {
+                    sb.append(x.charAt(i));
+                    if (dec)
+                        f *= 10;
+                }
+            res += Long.parseLong(sb.toString()) / f;
+            return res * (neg ? -1 : 1);
+        }
+        public long[] nextlongArray(int n) throws IOException {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextLong();
+            return a;
+        }
+        public Long[] nextLongArray(int n) throws IOException {
+            Long[] a = new Long[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextLong();
+            return a;
+        }
+        public int[] nextIntArray(int n) throws IOException {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+        public Integer[] nextIntegerArray(int n) throws IOException {
+            Integer[] a = new Integer[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+        public boolean ready() throws IOException {
+            return br.ready();
+        }
+
+    }
+}
