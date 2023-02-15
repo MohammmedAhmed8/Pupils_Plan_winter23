7ca22c131c263048f1bd2b6e77fbb8fb17d0a00a
MohammmedAhmed8 <94786411+MohammmedAhmed8@users.noreply.github.com>
2023-02-15 14:29:27 +0200

    Add files via upload

---
+++ Session 4 (Linear Data Structures)/sheet 4/E_CatchOverflow.java
@@ -0,0 +1,123 @@
+import java.util.*;
+import java.io.*;
+public class E_CatchOverflow{
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+        int n = sc.nextInt();
+        Stack<Long> st = new Stack<>();
+        Stack<Boolean> f = new Stack<>();
+        long x = 0 , lim = (1L << 32 ) - 1 , acc = 1;
+        f.push(false);
+        st.push(1L);
+        while(n-->0){
+            String s = sc.next();
+            if(s.equals("add")) {
+                x += st.peek();
+                if(f.peek() || x > lim){
+                    f.push(true);
+                    break;
+                }
+            }
+            else if(s.equals("for")){
+                long z = sc.nextInt();
+                st.push(z * st.peek());
+                f.push(f.peek() | st.peek() > lim);
+            }
+            else{
+                st.pop();
+                f.pop();
+            }
+        }
+        pw.println(!f.peek()? x : "OVERFLOW!!!");
+        pw.flush();
+    }
+    static class Scanner {
+
+        StringTokenizer st;
+        BufferedReader br;
+        public Scanner(InputStream s) {
+            br = new BufferedReader(new InputStreamReader(s));
+        }
+        public Scanner(String file) throws IOException {
+            br = new BufferedReader(new FileReader(file));
+        }
+        public Scanner(FileReader r) {
+            br = new BufferedReader(r);
+        }
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreTokens())
+                st = new StringTokenizer(br.readLine());
+            return st.nextToken();
+        }
+        public String readAllLines(BufferedReader reader) throws IOException {
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+                content.append(System.lineSeparator());
+            }
+            return content.toString();
+        }
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+        public String nextLine() throws IOException {
+            return br.readLine();
+        }
+        public double nextDouble() throws IOException {
+            String x = next();
+            StringBuilder sb = new StringBuilder("0");
+            double res = 0, f = 1;
+            boolean dec = false, neg = false;
+            int start = 0;
+            if (x.charAt(0) == '-') {
+                neg = true;
+                start++;
+            }
+            for (int i = start; i < x.length(); i++)
+                if (x.charAt(i) == '.') {
+                    res = Long.parseLong(sb.toString());
+                    sb = new StringBuilder("0");
+                    dec = true;
+                } else {
+                    sb.append(x.charAt(i));
+                    if (dec)
+                        f *= 10;
+                }
+            res += Long.parseLong(sb.toString()) / f;
+            return res * (neg ? -1 : 1);
+        }
+        public long[] nextlongArray(int n) throws IOException {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextLong();
+            return a;
+        }
+        public Long[] nextLongArray(int n) throws IOException {
+            Long[] a = new Long[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextLong();
+            return a;
+        }
+        public int[] nextIntArray(int n) throws IOException {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+        public Integer[] nextIntegerArray(int n) throws IOException {
+            Integer[] a = new Integer[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+        public boolean ready() throws IOException {
+            return br.ready();
+        }
+
+    }
+}
